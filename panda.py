1aed46d4c4d8dff52038821a4ac79cc4ffd4e454
evelyn-lg <lazaro.evelyn@outlook.com>
2025-12-24 00:39:58 -0600

    pandas tiernos 2

--- panda.py
+++ panda.py
@@ -1,4 +1,4 @@
 print('Este es el archivo panda.py')
 print('Esto es en la rama main y no se relaciona con otros trabajos')
 
-print('Los pandas son animales que comen bambú y son muy adorables.')
+print('Los pandas son animales que comen bambú y son muy tiernos.')
