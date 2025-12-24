21696bca7da574ba6778c0f23feeff783ed4e728
evelyn-lg <lazaro.evelyn@outlook.com>
2025-12-24 01:01:40 -0600

    login v2

--- login.py
+++ login.py
@@ -1,1 +1,8 @@
+USERNAME_DEFAULT = "admin"
+PASSWORD_DEFAULT = "password123"   
+
 print("¡Bienvenido al sistema de login!")
+
+print("Por favor, ingrese su nombre de usuario y contraseña para continuar.")
+username = input("Nombre de usuario: ")
+password = input("Contraseña: ")
