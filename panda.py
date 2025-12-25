331f9c9bd20ee30f386dabf05e8a47ad1fa63eff
evelyn-lg <lazaro.evelyn@outlook.com>
2025-12-25 16:30:33 -0600

    inicio del cuarto día

--- panda.py
+++ panda.py
@@ -2,3 +2,6 @@ print('Este es el archivo panda.py')
 print('Esto es en la rama main y no se relaciona con otros trabajos')
 
 print('Los pandas son animales que comen bambú y son muy tiernos.')
+
+print('Cuarto día aprendiendo Git y GitHub.')
+print('Los pandas viven principalmente en China y son un símbolo nacional allí.')
